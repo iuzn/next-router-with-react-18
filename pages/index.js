dbb94a5b2d8c5a0e561829a90a293ac079185c9b
Ibrahim Uzun <iuzun@marun.edu.tr>
2022-03-30 22:46:04 +0300

    update page

--- pages/index.js
+++ pages/index.js
@@ -2,31 +2,35 @@ import { useRouter } from 'next/router'
 import {useEffect, useState} from "react";
 
 export default function Home() {
-  const router = useRouter()
-  console.log(router.isReady)
-  const [isReady, setIsReady] = useState(null)
-  const [hasMounted, setHasMounted] = useState(false)
+    const router = useRouter()
 
-  // Blocking hydration warning for SSR
-  useEffect(() => {
-    setIsReady(router.isReady)
-  }, [router.isReady])
-  useEffect(() => {
-    setHasMounted(true)
-  }, [])
-  if (!hasMounted) {
-    return null
-  }
+    const [isReady, setIsReady] = useState(router.isReady)
+    const [hasMounted, setHasMounted] = useState(false)
 
-  return (
-      <div>
-       <span>
-        { isReady ? "Route is ready":"Route is not ready"}
-       </span>
-        <br/>
-        <button onClick={()=>router.push("/?id=random032mf").then(()=>router.reload())}>Add query to url</button>
-        <br/>
-        <button onClick={()=>router.push("/").then(()=>router.reload())}>Delete query from url</button>
-      </div>
-  )
+    // Set router.isReady when the page has mounted
+    useEffect(() => {
+        setIsReady(router.isReady)
+    }, [router])
+
+    // Blocking hydration warning
+    useEffect(() => {
+        setHasMounted(true)
+    }, [])
+    if (!hasMounted) {
+        return null
+    }
+
+    const addQuery = (query) => {
+        router.push({
+            pathname: '/',
+            query: query ? {query: query} : null
+        }).then(()=>router.reload())
+    }
+    return (
+        <div>
+        <h2>{ isReady ? "Route is ready" : "Route is not ready"}</h2>
+        <button onClick={()=>addQuery("1okok3mkn3mjf7f")}>Add query to url</button>
+        <button onClick={()=>addQuery(null)}>Delete query from url</button>
+        </div>
+    )
 }
